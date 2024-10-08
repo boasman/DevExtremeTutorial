3112099a14ef64f912961dea8b17d42a276e6c7f
elvis michael paulino <epaulino@rauroszm.com>
2024-10-08 15:26:05 +0200

    Se completo la parte de ButtonGroup

--- UI Components/Practica/src/app/components/button-group/button-group.component.ts
+++ UI Components/Practica/src/app/components/button-group/button-group.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { DxButtonGroupModule } from 'devextreme-angular';
 
 @Component({
@@ -6,10 +7,36 @@ import { DxButtonGroupModule } from 'devextreme-angular';
   standalone: true,
   imports: [
       DxButtonGroupModule
+      
   ],
   templateUrl: './button-group.component.html',
   styleUrl: './button-group.component.css'
 })
 export class ButtonGroupComponent {
+
+  fontStyles = [{
+    icon: "bold",
+    style: "bold"
+}, {
+    icon: "italic",
+    style: "italic"
+}, {
+    icon: "underline",
+    style: "underline"
+}, {
+    icon: "strike",
+    style: "strike"
+}];
+
+selectedNames: string[] = ['bold','italic']
+
+logSelectionChanged(e:any) {
+  if(e.addedItems[0]) {
+      console.log("The following style is added: " + e.addedItems[0].style);
+  }
+  if(e.removedItems[0]) {
+      console.log("The following style is removed: " + e.removedItems[0].style)
+  }
+}
 
 }
