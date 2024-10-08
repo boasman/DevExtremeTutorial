2da5a62068a5ad212d3385edf00095d68a5fe3f5
elvis michael paulino <epaulino@rauroszm.com>
2024-10-08 13:56:33 +0200

    Se eliminaron algunos proyectos

---
+++ UI Components/Practica/src/app/components/chech-box/chech-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chech-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './chech-box.component.html',
+  styleUrl: './chech-box.component.css'
+})
+export class ChechBoxComponent {
+
+}
